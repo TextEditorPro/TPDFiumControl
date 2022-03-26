255802aac78e8c85933f66cee92d106f44e733ba
Lasse Markus Rautiainen <lasse.markus.rautiainen@gmail.com>
2022-03-26 13:12:34 +0200

    Added load from url

--- Source/PDFium.Control.pas
+++ Source/PDFium.Control.pas
@@ -1,5 +1,7 @@
 ﻿unit PDFium.Control;
 
+{.$DEFINE USE_LOAD_FROM_URL}
+
 interface
 
 uses
@@ -159,6 +161,9 @@ type
     procedure GotoPreviousPage;
     procedure LoadFromFile(const AFilename: string);
     procedure LoadFromStream(const AStream: TStream);
+{$IFDEF USE_LOAD_FROM_URL}
+    procedure LoadFromURL(const AURL: string);
+{$ENDIF}
     procedure Print;
     procedure RotatePageClockwise;
     procedure RotatePageCounterClockwise;
@@ -223,6 +228,7 @@ implementation
 uses
   Winapi.ShellAPI, System.Character, System.Generics.Collections, System.Generics.Defaults, System.Types, Vcl.Clipbrd,
   Vcl.Printers
+{$IFDEF USE_LOAD_FROM_URL}, IdHTTP, IdSSLOpenSSL{$ENDIF}
 {$IFDEF ALPHASKINS}, sConst, sDialogs, sMessages, sStyleSimply, sVCLUtils{$ENDIF};
 
 const
@@ -439,6 +445,7 @@ begin
     else
       ShowError(E.Message);
   end;
+
   AfterLoad;
 end;
 
@@ -465,8 +472,42 @@ begin
     else
       ShowError(E.Message);
   end;
+
   AfterLoad;
 end;
+
+{$IFDEF USE_LOAD_FROM_URL}
+procedure TPDFiumControl.LoadFromURL(const AURL: string);
+var
+  LStream: TMemoryStream;
+  LHTTPClient: TIdHTTP;
+begin
+  LHTTPClient := TIdHTTP.Create;
+  try
+    LStream := TMemoryStream.Create;
+    try
+      LHTTPClient.ReadTimeout := 60000;
+      if Pos('https://', AURL) = 1 then
+      begin
+        LHTTPClient.IOHandler := TIdSSLIOHandlerSocketOpenSSL.Create(LHTTPClient);
+        with TIdSSLIOHandlerSocketOpenSSL(LHTTPClient.IOHandler).SSLOptions do
+        begin
+          Method := sslvTLSv1_2;
+          SSLVersions := SSLVersions + [sslvTLSv1_2];
+        end;
+        LHTTPClient.Request.BasicAuthentication := True;
+      end;
+      LHTTPClient.Get(AURL, LStream);
+      LStream.Position := 0;
+      LoadFromStream(LStream);
+    finally
+      FreeAndNil(LStream);
+    end;
+  finally
+    LHTTPClient.Free;
+  end;
+end;
+{$ENDIF}
 
 procedure TPDFiumControl.AfterLoad;
 begin
@@ -862,6 +903,11 @@ begin
   Result := FPageIndex + 1;
 end;
 
+function TPDFiumControl.PageToScreen(const AValue: Single): Integer;
+begin
+  Result := Round(AValue * ZoomToScreen);
+end;
+
 function TPDFiumControl.GetPageTop(const APageIndex: Integer): Integer;
 var
   LY: Double;
@@ -897,7 +943,7 @@ procedure TPDFiumControl.AdjustPageInfo;
 var
   LIndex: Integer;
   LTop: Double;
-  LZoom: Double;
+  LScale: Double;
   LClient: TRect;
   LRect: TRect;
   LMargin: Integer;
@@ -908,13 +954,13 @@ begin
   LClient := ClientRect;
   LTop := 0;
   LMargin := FPageMargin;
-  LZoom := FZoomPercent / 100 * Screen.PixelsPerInch / 72;
+  LScale := FZoomPercent / 100 * Screen.PixelsPerInch / 72;
   for LIndex := 0 to FPageCount - 1 do
   begin
-    LRect.Top := Round(LTop * LZoom) + LMargin - VertScrollBar.Position;
-    LRect.Left := FPageMargin + Round((FWidth - FPageInfo[LIndex].Width) / 2 * LZoom) - HorzScrollBar.Position;
-    LRect.Width := Round(FPageInfo[LIndex].Width * LZoom);
-    LRect.Height := Round(FPageInfo[LIndex].Height * LZoom);
+    LRect.Top := Round(LTop * LScale) + LMargin - VertScrollBar.Position;
+    LRect.Left := FPageMargin + Round((FWidth - FPageInfo[LIndex].Width) / 2 * LScale) - HorzScrollBar.Position;
+    LRect.Width := Round(FPageInfo[LIndex].Width * LScale);
+    LRect.Height := Round(FPageInfo[LIndex].Height * LScale);
     if LRect.Width < LClient.Width - 2 * FPageMargin then
       LRect.Offset((LClient.Width - LRect.Width) div 2 - LRect.Left, 0);
 
@@ -1295,11 +1341,6 @@ begin
 
   LScale := 72 / Screen.PixelsPerInch;
   Result := 100 * (ClientWidth - 2 * FPageMargin) * LScale / FPageInfo[FPageIndex].Width;
-end;
-
-function TPDFiumControl.PageToScreen(const AValue: Single): Integer;
-begin
-  Result := Round(AValue * ZoomToScreen);
 end;
 
 function TPDFiumControl.SetSelStopCharIndex(const X, Y: Integer): Boolean;
@@ -1937,6 +1978,11 @@ begin
         LFromPage := LPrintDialog.FromPage;
         LToPage := LPrintDialog.ToPage;
       end;
+      { Note! Copies and collate won't work. Andy's core class needs to be fixed to get it working.
+        Capture here the variables and pass them to following Print function.
+
+        LCopies := LPrintDialog.Copies;
+        LCollate := LPrintDialog.Collate; }
     finally
       LPrintDialog.Free;
     end;
