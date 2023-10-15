947e62b65f5184f05eceb6de3818752f7b3c9a52
Lasse Markus Rautiainen <lasse.markus.rautiainen@gmail.com>
2023-10-15 12:47:28 +0300

    Update

--- README.md
+++ README.md
@@ -16,6 +16,99 @@ Define | Description
 ------ | -----------
 ALPHASKINS | Native AlphaSkins support
 
+## Annotation links
+
+Use of annotation links requires a following fix for PdfiumCore.pas.
+```Pascal
+TPdfAnnotation = class(TObject)
+private
+  FFormField: TPdfFormField;
+  FHandle: FPDF_ANNOTATION;
+  FIsFormField: Boolean;
+  FIsLink: Boolean;
+  FLinkRect: TPDFRect;
+  FPage: TPdfPage;
+  FURL: string;
+protected
+  constructor Create(APage: TPdfPage; AHandle: FPDF_ANNOTATION);
+public
+  destructor Destroy; override;
+  property FormField: TPdfFormField read FFormField;
+  property Handle: FPDF_ANNOTATION read FHandle;
+  property IsFormField: Boolean read FIsFormField;
+  property IsLink: Boolean read FIsLink;
+  property LinkRect: TPDFRect read FLinkRect;
+  property URL: string read FURL;
+end;
+
+constructor TPdfAnnotation.Create(APage: TPdfPage; AHandle: FPDF_ANNOTATION);
+var
+  LSubType: FPDF_ANNOTATION_SUBTYPE;
+  LRect: FS_RECTF;
+  LLink: FPDF_LINK;
+  LAction: FPDF_ACTION;
+  LActionType: LongWord;
+  LBufferSize: LongWord;
+  LBuffer: array of AnsiChar;
+begin
+  inherited Create;
+
+  FPage := APage;
+  FHandle := AHandle;
+
+  LSubType := FPDFAnnot_GetSubtype(AHandle);
+
+  FIsFormField := LSubType in [FPDF_ANNOT_WIDGET, FPDF_ANNOT_XFAWIDGET];
+
+  if FIsFormField then
+    FFormField := TPdfFormField.Create(Self);
+
+  FIsLink := LSubType = FPDF_ANNOT_LINK;
+
+  if FIsLink then
+  begin
+    FPDFAnnot_GetRect(AHandle, @LRect);
+
+    FLinkRect.Left := LRect.Left;
+    FLinkRect.Top := LRect.Top;
+    FLinkRect.Right := LRect.Right;
+    FLinkRect.Bottom := LRect.Bottom;
+
+    LLink := FPDFAnnot_GetLink(AHandle);
+
+    if Assigned(LLink) then
+    begin
+      LAction := FPDFLink_GetAction(LLink);
+      LActionType := FPDFAction_GetType(LAction);
+
+      if LActionType = PDFACTION_URI then
+      begin
+        LBufferSize := FPDFAction_GetURIPath(APage.FDocument.FDocument, LAction, nil, 0);
+        SetLength(LBuffer, LBufferSize);
+        FPDFAction_GetURIPath(APage.FDocument.FDocument, LAction, PAnsiChar(LBuffer), LBufferSize);
+        SetString(FURL, PAnsiChar(LBuffer), LBufferSize);
+      end;
+    end;
+  end;
+end;
+
+destructor TPdfAnnotation.Destroy;
+begin
+  if Assigned(FFormField) then
+    FreeAndNil(FFormField);
+
+  if FHandle <> nil then
+  begin
+    FPDFPage_CloseAnnot(FHandle);
+    FHandle := nil;
+  end;
+
+  if FPage.FAnnotations <> nil then
+    FPage.FAnnotations.DestroyingItem(Self);
+
+  inherited Destroy;
+end;
+```
 ## License
 
 [MIT](https://github.com/TextEditorPro/TTextEditor/blob/main/LICENSE)
--- Source/PDFium.Control.pas
+++ Source/PDFium.Control.pas
@@ -1,98 +1,7 @@
 ﻿unit PDFium.Control;
 
 {.$DEFINE USE_LOAD_FROM_URL}
-{.$DEFINE USE_ANNOTATION_LINKS}
-
-{ Use of annotation links requires a following fix for PdfiumCore.pas
-
-  TPdfAnnotation = class(TObject)
-  private
-    FFormField: TPdfFormField;
-    FHandle: FPDF_ANNOTATION;
-    FIsFormField: Boolean;
-    FIsLink: Boolean;
-    FLinkRect: TPDFRect;
-    FPage: TPdfPage;
-    FURL: string;
-  protected
-    constructor Create(APage: TPdfPage; AHandle: FPDF_ANNOTATION);
-  public
-    destructor Destroy; override;
-    property FormField: TPdfFormField read FFormField;
-    property Handle: FPDF_ANNOTATION read FHandle;
-    property IsFormField: Boolean read FIsFormField;
-    property IsLink: Boolean read FIsLink;
-    property LinkRect: TPDFRect read FLinkRect;
-    property URL: string read FURL;
-  end;
-
-  constructor TPdfAnnotation.Create(APage: TPdfPage; AHandle: FPDF_ANNOTATION);
-  var
-    LSubType: FPDF_ANNOTATION_SUBTYPE;
-    LRect: FS_RECTF;
-    LLink: FPDF_LINK;
-    LAction: FPDF_ACTION;
-    LActionType: LongWord;
-    LBufferSize: LongWord;
-    LBuffer: array of AnsiChar;
-  begin
-    inherited Create;
-
-    FPage := APage;
-    FHandle := AHandle;
-
-    LSubType := FPDFAnnot_GetSubtype(AHandle);
-
-    FIsFormField := LSubType in [FPDF_ANNOT_WIDGET, FPDF_ANNOT_XFAWIDGET];
-
-    if FIsFormField then
-      FFormField := TPdfFormField.Create(Self);
-
-    FIsLink := LSubType = FPDF_ANNOT_LINK;
-
-    if FIsLink then
-    begin
-      FPDFAnnot_GetRect(AHandle, @LRect);
-
-      FLinkRect.Left := LRect.Left;
-      FLinkRect.Top := LRect.Top;
-      FLinkRect.Right := LRect.Right;
-      FLinkRect.Bottom := LRect.Bottom;
-
-      LLink := FPDFAnnot_GetLink(AHandle);
-
-      if Assigned(LLink) then
-      begin
-        LAction := FPDFLink_GetAction(LLink);
-        LActionType := FPDFAction_GetType(LAction);
-
-        if LActionType = PDFACTION_URI then
-        begin
-          LBufferSize := FPDFAction_GetURIPath(APage.FDocument.FDocument, LAction, nil, 0);
-          SetLength(LBuffer, LBufferSize);
-          FPDFAction_GetURIPath(APage.FDocument.FDocument, LAction, PAnsiChar(LBuffer), LBufferSize);
-          SetString(FURL, PAnsiChar(LBuffer), LBufferSize);
-        end;
-      end;
-    end;
-  end;
-
-  destructor TPdfAnnotation.Destroy;
-  begin
-    if Assigned(FFormField) then
-      FreeAndNil(FFormField);
-
-    if FHandle <> nil then
-    begin
-      FPDFPage_CloseAnnot(FHandle);
-      FHandle := nil;
-    end;
-
-    if FPage.FAnnotations <> nil then
-      FPage.FAnnotations.DestroyingItem(Self);
-
-    inherited Destroy;
-  end; }
+{.$DEFINE USE_ANNOTATION_LINKS} // Use of annotation links requires a fix for PdfiumCore.pas, see README.md
 
 interface
 
@@ -1721,11 +1630,11 @@ begin
       begin
         LPoint := DeviceToPage(X, Y);
 
-        if IsWebLinkAt(X, Y) then
+        if Assigned(FOnClickLink) and IsWebLinkAt(X, Y) then
           LCursor := crHandPoint
         else
 {$IFDEF USE_ANNOTATION_LINKS}
-        if IsAnnotationLinkAt(X, Y, LURL, LRect) then
+        if Assigned(FOnClickLink) and IsAnnotationLinkAt(X, Y, LURL, LRect) then
         begin
           LCursor := crHandPoint;
           ShowHint(LURL, LRect);
@@ -1787,12 +1696,12 @@ begin
 
     if not FSelectionActive then
     begin
-      if IsWebLinkAt(X, Y, LURL) then
-        if Assigned(FOnClickLink) then
+      if Assigned(FOnClickLink) then
+        if IsWebLinkAt(X, Y, LURL) then
           FOnClickLink(Self, LURL);
 {$IFDEF USE_ANNOTATION_LINKS}
-      if IsAnnotationLinkAt(X, Y, LURL, LRect) then
-        if Assigned(FOnClickLink) then
+      if Assigned(FOnClickLink) then
+        if IsAnnotationLinkAt(X, Y, LURL, LRect) then
           FOnClickLink(Self, LURL);
 {$ENDIF}
     end;
